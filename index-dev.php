d6ff6671097b3169804c482d4ce6761cbb789eba
bozerkins <bozerkins@gmail.com>
2015-06-21 19:27:14 +0300

    basic structure

--- index-dev.php
+++ index-dev.php
@@ -1,3 +1,23 @@
 <?php
 
-echo 'Hello world! - dev';
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PureGlassAnalytics\HttpFoundation\Request;
+use PureGlassAnalytics\Router\Router;
+use PureGlassAnalytics\Router\RouterQuery;
+
+// create the Request object
+$request = Request::createFromGlobals();
+
+// create The Router query object
+$routerQuery = new RouterQuery();
+$routerQuery->setRequest($request);
+
+// create the Router
+$router = new Router();
+
+// resolve requested route
+$response = $router->resolve('development', $routerQuery);
+
+// send the result
+$response->send();
