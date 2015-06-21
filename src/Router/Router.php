080955f4de1a37072949ae6e6ac43be8619aac8d
bozerkins <bozerkins@gmail.com>
2015-06-21 22:13:25 +0300

    services loading logics

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -10,18 +10,20 @@ class Router
 {
 	public function resolve($environment, RouterQuery $query)
 	{
+		// create apoplication Configuration
 		$config = new Config();
 		$config->setEnvironment($environment);
 
+		// create application Container (currently Singleton)
 		$container = Container::getInstance();
+		// add Configuration service to container
 		$container->set('config', $config);
+		// create lazy loading services from conviguration
 		$container->addRegistry($container->get('config')->get('services'));
 
-		$container->get('database');
+
 
 		throw new \ErrorException('Write ROUTER LOGICS');
-
-
 
 		$response = new Response('Default message');
 		return $response;
